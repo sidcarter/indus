279207dd96ac07738515b2e5ec9f9b0054bcf853
sidcarter <geek@sidcarter.com>
2014-11-28 21:04:53 -0500

    Added policy and list file functions.

--- scripts/s3.py
+++ scripts/s3.py
@@ -16,11 +16,18 @@ def list_buckets():
 
 def list_files():
 	bucketname=raw_input("Which bucket's files you want? ")
+	count=0
 	if (conn.lookup(bucketname)):
 		files=conn.get_bucket(bucketname).get_all_keys()
 		print "Files: "
 		for k in files:
+			count+=1
 			print k.name.encode('utf-8')
+		print ("\nTotal number of files: "+str(count))
+		choice=raw_input("Would you like to download these files? ")
+		if (choice=="n"): return
+		else:
+			print ("Downloading files...")
 	else:
 		print "No such bucket found."
 		return
@@ -31,7 +38,7 @@ def list_policy():
 		policy=conn.get_bucket(bucketname).get_policy()
 		print ("Policy for Bucket "+bucketname+": ")
 		print policy
-	except S3ResponseError:
+	except:
 		print ("Policy or Bucket not found.")
 	
 def bucket_delete():
@@ -53,11 +60,11 @@ def bucket_delete():
 		conn.delete_buckets(bucket_to_delete)	
 		print ('Terminated '+bucket_to_delete+'  successfully')
 
-if ((len(sys.argv) > 1) and(sys.argv[1] == "term")) :
-	bucket_delete()
-elif ((len(sys.argv) > 1) and(sys.argv[1] == "policy")) :
+if ((len(sys.argv) > 1) and(sys.argv[1] == "-p")) :
 	list_policy()
-elif ((len(sys.argv) > 1) and(sys.argv[1] == "files")) :
+elif ((len(sys.argv) > 1) and(sys.argv[1] == "-lf")) :
+	list_files()
+elif ((len(sys.argv) > 1) and(sys.argv[1] == "-gf")) :
 	list_files()
 else:
 	list_buckets()
