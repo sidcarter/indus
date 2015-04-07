b2455ac69092e56c44f62eb247e589d970234f6d
Sid Carter <me@sidcarter.com>
2015-04-07 15:01:28 -0400

    Made the script a bit more flexible

--- scripts/sqs.py
+++ scripts/sqs.py
@@ -1,17 +1,27 @@
 #!/usr/bin/env python
+import sys
 import boto.sqs
+
+if (len(sys.argv) <= 2):
+	sys.exit("Insufficient arguments.\nUsage: sqs.py <queue_name> <no_of_messages>")
+else:
+	que_name = sys.argv[1]
+	no_of_messages = sys.argv[2]
 
 conn = boto.sqs.connect_to_region("us-east-1")
 
-all_ques = conn.get_all_queues()
+# all_ques = conn.get_all_queues()
+#for queue in all_ques:
+#	print("Queue: %s" % queue)
 
-for queue in all_ques:
-	print("Queue: %s" % queue)
+bounce_que = conn.get_queue(que_name)
 
-bounce_que = conn.get_queue("ses-bounces")
+try:
+	rs = bounce_que.get_messages(no_of_messages)
+except AttributeError as e:
+	print "Error: %s\nIt's possible that you provided an invalid queue name." % e
+else:
+	print("We are looking at %s messages." % len(rs))
 
-rs = bounce_que.get_messages(5)
-print("We are looking at about %s messages." % len(rs))
-
-for i in range(len(rs)):
-	print(rs[i].get_body())
+	for i in range(len(rs)):
+		print(rs[i].get_body())
