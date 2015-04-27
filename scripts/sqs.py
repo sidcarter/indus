74edb886e49b8351883ab9d2f52ad3dc26aa1c78
Sid Carter <me@sidcarter.com>
2015-04-27 00:18:59 -0400

    update sqs to output just the message

--- scripts/sqs.py
+++ scripts/sqs.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import sys,time
+import json
 import boto.sqs
 
 if (len(sys.argv) <= 1):
@@ -14,17 +15,21 @@ conn = boto.sqs.connect_to_region("us-east-1")
 #for queue in all_ques:
 #	print("Queue: %s" % queue)
 
-bounce_que = conn.get_queue(que_name)
+que = conn.get_queue(que_name)
 
 try:
-	rs = bounce_que.get_messages(no_of_messages)
+	rs = que.get_messages(no_of_messages)
 except AttributeError as e:
-	print "Error: %s\nIt's possible that you provided an invalid queue name." % e
+	print "Error: %s\nInvalid queue name." % e
 else:
-	print("We are looking at %s messages." % len(rs))
+	
+	if len(rs)!= 0:
 
-	while True:
-		for i in range(len(rs)):
-			print(rs[i].get_body())
-			conn.delete_message(bounce_que,rs[i])
-		time.sleep(1)
+		while True:
+			for i in range(len(rs)):
+				message = json.loads(rs[i].get_body())['Message']
+				print(message)
+#				conn.delete_message(que,rs[i])
+			time.sleep(1)
+	else:
+		sys.exit("No messages to process")
