138af6694260723b72243bf164bc5e8c19edce5e
Sid Carter <me@sidcarter.com>
2015-05-13 23:41:31 -0400

    making the ec2 script a bit more versatile

--- scripts/ec2.py
+++ scripts/ec2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import sys, traceback
 from argparse import ArgumentParser
-from boto import ec2 as ec2
+from boto import ec2
 
 def ec2_info(region="us-east-1"):
     conn = ec2.connect_to_region(region)
@@ -56,15 +56,19 @@ def ec2_start_stop():
 	except Exception as e:
 		print (e)
 		
-def ec2_secgroups(region="us-east-1", file=None,sg=None):
+def ec2_secgroups(region="us-east-1", filename=None,sg=None):
         try:
             conn=ec2.connect_to_region(region)
-            rs=conn.get_all_security_groups()
+            sgs=conn.get_all_security_groups()
+            if filename: 
+                file = open(filename,'r')
+            else:
+                file = None
         except Exception as e:
             sys.exit(e)
 
         if not file:
-            for item in rs:
+            for item in sgs:
                 print "Rules: for %s are:" % item.name
                 for rule in item.rules:
                     for grant in rule.grants:
@@ -76,10 +80,10 @@ def ec2_secgroups(region="us-east-1", file=None,sg=None):
                             print "\t%s-%s (%s)" % (rule.from_port, rule.to_port, grant.name)
                 print
         elif file and sg:
-            
-            # the file should come in a comma separate file - cidr, from_port and to_port
+            # the file should be a comma separate file - cidr, from_port and to_port
+            # if we can't split a file we throw an error
             secgroup=None
-            for item in rs:
+            for item in sgs:
                 if item.name==sg: 
                     secgroup=item
             if secgroup:
@@ -89,16 +93,19 @@ def ec2_secgroups(region="us-east-1", file=None,sg=None):
                         (source_cidr,from_port,to_port) = line.split(",") 
                         print("%s %s %s %s") %(source_cidr, from_port, to_port ,sg)
                         secgroup.authorize(ip_protocol="tcp",from_port=from_port, to_port=to_port, cidr_ip=source_cidr)
-                    except:
+                    except ValueError:
                         sys.exit(traceback.format_exc())
+                    finally:
+                        file.close()
             else: 
                 print "Security Group: %s not found" % sg
             
 def main():
     
     parser = ArgumentParser(description='Custom AWS Interface', prog="ec2.py")
-    parser.add_argument('-r', '--read-file', nargs=1, required=False, help='input file to be provided')
+    parser.add_argument('-i', '--input-file', nargs=1, required=False, help='input file to be provided')
     parser.add_argument('-sg', '--security-group', nargs=1, required=False, help='security group to be changed or modified')
+    parser.add_argument('-u', '--update', nargs=1, choices=['instances','security_groups'], help='entities to be changed or modified e.g. -u instances')    
     parser.add_argument('-l', '--list', nargs=1, choices=['instances','security_groups'], help='list all entities e.g. -l instances')
     args=parser.parse_args()
     
@@ -107,18 +114,11 @@ def main():
             ec2_info()
         elif args.list[0]=="security_groups":
             ec2_secgroups()
-    elif args.read_file:
-        if args.security_group:
-            filename=args.read_file[0]
-            security_group=args.security_group[0]
-            try:
-                f = open(filename,'r')
-                ec2_secgroups(file=f,sg=security_group)
-            except Exception as e:
-                print(e)
-        else:
-            print("Please provide a security group to modify.")
-    
-#    ec2_info("all")
+    elif args.update and args.input_file and args.security_group:
+        filename=args.input_file[0]
+        security_group=args.security_group[0]
+        ec2_secgroups(filename=filename,sg=security_group)
+    else:
+        print("Not enough arguments or nothing for me to do, really.")
     
 main()
