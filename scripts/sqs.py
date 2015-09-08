459e8aa4dd40b438b80e02f990cd9805a715b58d
Sid Carter <me@sidcarter.com>
2015-09-08 11:40:31 -0400

    get just email addresses from ses-queues

--- scripts/sqs.py
+++ scripts/sqs.py
@@ -3,33 +3,40 @@ import sys,time
 import json
 import boto.sqs
 
-if (len(sys.argv) <= 1):
-	sys.exit("Insufficient arguments.\nUsage: sqs.py <queue_name> <no_of_messages>")
-else:
-	que_name = sys.argv[1]
-	no_of_messages = 10
+def get_args():
+    if (len(sys.argv) <= 1):
+        sys.exit("Insufficient arguments.\nUsage: sqs.py <queue_name> <no_of_messages>")
+    else:
+        que_name = sys.argv[1]
+        no_of_messages = sys.argv[2] if len(sys.argv)>2 else 10
 
-conn = boto.sqs.connect_to_region("us-east-1")
+    return (que_name,no_of_messages)
 
-# all_ques = conn.get_all_queues()
-#for queue in all_ques:
-#	print("Queue: %s" % queue)
+def process_ques(que_name,no_of_messages):
+    conn = boto.sqs.connect_to_region("us-east-1")
 
-que = conn.get_queue(que_name)
+    # all_ques = conn.get_all_queues()
+    #for queue in all_ques:
+    #	print("Queue: %s" % queue)
 
-try:
-	rs = que.get_messages(no_of_messages)
-except AttributeError as e:
-	print "Error: %s\nInvalid queue name." % e
-else:
-	
-	if len(rs)!= 0:
+    que = conn.get_queue(que_name)
 
-		while True:
-			for i in range(len(rs)):
-				message = json.loads(rs[i].get_body())['Message']
-				print(message)
-#				conn.delete_message(que,rs[i])
-			time.sleep(1)
-	else:
-		sys.exit("No messages to process")
+    try:
+	    rs = que.get_messages(no_of_messages)
+    except AttributeError as e:
+	    print "Error: %s\nInvalid queue name." % e
+    else:
+        if len(rs)!= 0:
+            while True:
+                for i in range(len(rs)):
+                    message = json.loads(rs[i].get_body())['Message']
+                    print(json.loads(message))['bounce']['bouncedRecipients'][0]['emailAddress']
+                    # print(message['bounce'])
+                    conn.delete_message(que,rs[i])
+                time.sleep(1)
+        else:
+			sys.exit("No messages to process")
+
+if __name__=="__main__":
+    que_name,no_of_messages=get_args()
+    process_ques(que_name,no_of_messages)
